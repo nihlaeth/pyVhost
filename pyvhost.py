e8c567061b2c90b564e4f35812684dc8e012bf83
nihlaeth <info@nihlaeth.nl>
2015-11-09 15:00:32 +0100

    Separate log function to its own module

--- pyvhost.py
+++ pyvhost.py
@@ -10,6 +10,8 @@ import os
 import errno
 import random
 
+from colorlog import log
+
 import MySQLdb as db
 import gnupg
 from email.mime.text import MIMEText
@@ -47,30 +49,6 @@ def cap16(text):
         return text[0:16]
     else:
         return text
-
-
-def log(lvl, msg):
-    """Log data (with pretty colors)."""
-    header = '\033[95m'
-    okblue = '\033[94m'
-    okgreen = '\033[32m'  # '\033[92m'
-    warning = '\033[36m'  # '\033[93m'
-    fail = '\033[91m'
-    endc = '\033[0m'
-    # bold = "\033[1m"
-    if lvl == "info":
-        print okblue + str(msg) + endc
-    elif lvl == "header":
-        print header + str(msg) + endc
-    elif lvl == "ok":
-        print okgreen + str(msg) + endc
-    elif lvl == "fail":
-        print fail + str(msg) + endc
-    elif lvl == "warn":
-        print warning + str(msg) + endc
-    else:
-        print fail + "unknown log type"
-        print str(msg) + endc
 
 
 # pylint: disable=too-many-instance-attributes
